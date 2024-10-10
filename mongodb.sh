077013c998501c1a8747e05ba33249e90be10abc
Mohammed <i23281@verisk.com>
2024-10-09 21:11:23 -0400

    mongodb and repo change is coded

---
+++ mongodb.sh
@@ -0,0 +1,9 @@
+cp mongo.repo /etc/yum.repos.d/mongodb.repo
+
+ dnf install mongodb-org -y
+
+sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf
+
+ systemctl enable mongod
+ systemctl start mongod
+
