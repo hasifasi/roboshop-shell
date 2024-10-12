9938eb7cd020c3eb6e8ea9a7ecad1ae96fb6b553
Mohammed <i23281@verisk.com>
2024-10-11 20:17:44 -0400

    commit user.service user.sh

--- redis.sh
+++ redis.sh
@@ -8,4 +8,4 @@ dnf install redis -y
 sed -i -e 's/127.0.0.0/0.0.0.0/' -e '/protected-mode/  c protected- mode no'   /etc/redis/redis.conf
 
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
