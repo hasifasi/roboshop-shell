4af280a1728fac1255033f213597c58cc0146ef2
Mohammed <i23281@verisk.com>
2024-10-14 00:47:18 -0400

    Commit Variables

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,28 @@
+color="\e[33m"
+noColor="\e[0m"
 
-echo -e "\e[31m check and install nginx \e[0m"
+
+echo -e "$color check and install nginx $noColor"
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 
-echo -e "\e[31m Placing the nginx.conf \e[0m"
+echo -e "$color Placing the nginx.conf $noColor"
 cp nginx.conf /etc/nginx/nginx.conf
 
-echo -e "\e[31m enable and start nginx \e[0m"
+echo -e "$color enable and start nginx $noColor"
 systemctl enable nginx
 systemctl start nginx
 
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[31m Download the dev code and place it in tmp folder \e[0m"
+echo -e "$color Download the dev code and place it in tmp folder $noColor"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
-echo -e "\e[31m Unzip the dev code from tmp folder \e[0m"
+echo -e "$color Unzip the dev code from tmp folder $noColor"
 # shellcheck disable=SC2164
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[31m Restart the nginx \e[0m"
+echo -e "$color Restart the nginx $noColor"
 systemctl restart nginx
