f4e6a0ae0e99730c1817799aa1c92b63935cb0d6
Mohammed <i23281@verisk.com>
2024-10-14 03:07:52 -0400

    Adding more functions.sh

--- common.sh
+++ common.sh
@@ -4,8 +4,8 @@
 
 color="\e[36m"
 noColor="\e[0m"
-logFile=/tmp/roboshiop.log   # Creating a variable to store all logs
-rm -f $logFile               # Remove the file before every new run
+logFile=/tmp/roboshop.log   # Creating a variable to store all logs
+rm -f $logFile              # Remove the file before every new run
 
 
 #Function to add user and create directory and Download Dev code to tmp and Unzip the same
